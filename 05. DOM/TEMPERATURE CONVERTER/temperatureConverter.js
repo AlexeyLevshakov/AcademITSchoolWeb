13dc7e7b11a9907536f7cde76d9d2961eb1e3fba
AlexeyLevshakov <sinistral@inbox.ru>
2019-03-19 14:43:42 +0700

    Сделал задачу "Перевод температур"

---
+++ 05. DOM/TEMPERATURE CONVERTER/temperatureConverter.js
@@ -0,0 +1,39 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function () {
+    var conversionButton = document.querySelector(".conversion-button");
+    conversionButton.addEventListener("click", function() {
+        var inputTemperature = document.querySelector(".input-temperature");
+        var kelvins = document.querySelector(".kelvins");
+        var degreesFahrenheit = document.querySelector(".degrees-Fahrenheit");
+
+        var temperatureDegreesCelsius = +inputTemperature.value;
+
+        var errorNaN = document.querySelector(".error-NaN");
+        errorNaN.setAttribute("style", "display: none");
+        var errorBelowAbsoluteZero = document.querySelector(".error-below-absolute-zero");
+        errorBelowAbsoluteZero.setAttribute("style", "display: none");
+
+        if(isNaN(temperatureDegreesCelsius)) {
+            errorNaN.setAttribute("style", "display: block");
+            kelvins.innerText = "";
+            degreesFahrenheit.innerText = "";
+            return;
+        }
+
+        var ABSOLUTE_ZERO_CELSIUS = -273.15;
+        var EPSILON = 1.0e-10;
+        if (ABSOLUTE_ZERO_CELSIUS - temperatureDegreesCelsius > EPSILON ) {
+            errorBelowAbsoluteZero.setAttribute("style", "display: block");
+            kelvins.innerText = "";
+            degreesFahrenheit.innerText = "";
+            return;
+        }
+
+        var temperatureKelvins = Math.round((temperatureDegreesCelsius + 273.15) * 100) / 100;
+        kelvins.innerText = temperatureKelvins;
+
+        var temperatureFahrenheit = Math.round((temperatureDegreesCelsius * 1.8 + 32) * 100) / 100;
+        degreesFahrenheit.innerText = temperatureFahrenheit;
+    });
+});
